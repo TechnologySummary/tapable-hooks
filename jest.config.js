60b7267c674089bc8099499440115beb35694e49
jaylen.chan <jaylen.chan@hotmail.com>
2022-05-17 20:50:12 +0800

    feat: tapable-hooks init

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  automock: false,
+  clearMocks: true,
+  testEnvironment: 'jsdom',
+  coverageProvider: 'v8',
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)']
+}
